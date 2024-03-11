21bf2715fade3f1ecf36c77da1fdb35047f923f4
ilona <gogiashvili.ilona@gmail.com>
2024-03-10 23:59:47 -0400

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1= User.create(name: "ilona",email: "ilona@gmail.com" )
+user2= User.create(name: "ilia", email: "ilia@gmail.com")
+user3= User.create(name: "sophia", email: "sophia@gmail.com")
+
+place1= Place.create(name: "New York")
+place2= Place.create(name: "Boston")
+place3 = Place.create(name: "Rockville")
